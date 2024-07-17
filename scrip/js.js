de068b9c01556147be0b850cd185123eba3419cd
bokien246 <vumihao@gmail.com>
2024-07-18 01:10:58 +0700

    first commit

--- scrip/js.js
+++ scrip/js.js
@@ -1,34 +1,6 @@
-let imageHashMap = JSON.parse(localStorage.getItem('imageHashMap')) || {};
+document.querySelectorAll('.thumbnail-images img').forEach(img => {
+    img.addEventListener('click', function() {
+        document.querySelector('.main-image').src = this.src;
+    });
+});
 
-function convertAndHashImage() {
-    const file = document.getElementById('fileInput').files[0];
-    const reader = new FileReader();
-    reader.onload = function() {
-        const base64 = reader.result.split(',')[1]; // Lấy phần Base64 của hình ảnh
-        const hash = CryptoJS.SHA256(base64).toString(CryptoJS.enc.Hex);
-        document.getElementById('hashOutput').value = hash;
-        imageHashMap[hash] = reader.result; // Lưu trữ hash và hình ảnh tương ứng
-
-        // Lưu imageHashMap vào localStorage
-        localStorage.setItem('imageHashMap', JSON.stringify(imageHashMap));
-    };
-    reader.readAsDataURL(file);
-}
-
-function lookupImage() {
-    const hash = document.getElementById('hashInput').value;
-    if (imageHashMap[hash]) {
-        document.getElementById('imageOutput').src = imageHashMap[hash];
-    } else {
-        alert('Hash không hợp lệ hoặc không có hình ảnh tương ứng.');
-    }
-}
-var catEl = document.getElementById("cat");
-var startTime = new Date().getTime();
-
-var walkTheCat = function() {
-    var currTime = new Date().getTime();
-    var secondsElapsed = ((currTime - startTime)/1000);
-};
-
-walkTheCat();
